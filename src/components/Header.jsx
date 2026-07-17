ed6963908f5b7d53c6980542745f7df788f7c2b1
Jatin Saini <jatinrohitsaini@gmail.com>
2026-07-17 20:16:35 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <h1>Hi, I'm Jatin</h1>
+            <p>Building DevOnboard to Solve developer Onboarding</p>
+            <a href="mailto:jatinrsaini@gmail.com">jatinrsaini@gmail.com</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
